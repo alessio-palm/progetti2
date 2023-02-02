3cd8d8206c181eb0b0a828563f50d72ecf2d727c
alessio-palm <123393341+alessio-palm@users.noreply.github.com>
2023-02-02 18:43:26 +0100

    prova

---
+++ index.js
@@ -0,0 +1,526 @@
+// AGGIUNGO IL LISTENER
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#avvio").addEventListener("click", inizio);
+    
+});
+
+    const immagine = document.createElement("img")
+        immagine.src= "img/1.png"
+        immagine.className = "card"
+        immagine.id= "1d"
+
+        const immagine2 = document.createElement("img")
+        immagine2.src= "img/2.png"
+        immagine2.className = "card"
+        immagine2.id= "2d"
+
+
+        const immagine3 = document.createElement("img")
+        immagine3.src= "img/3.png"
+        immagine3.className = "card"
+        immagine3.id= "3d"
+
+        const immagine4 = document.createElement("img")
+        immagine4.src= "img/4.png"
+        immagine4.className = "card"
+        immagine4.id= "4d"
+
+        const immagine5 = document.createElement("img")
+        immagine5.src= "img/5.png"
+        immagine5.className = "card"
+        immagine5.id= "5d"
+
+        const immagine6 = document.createElement("img")
+        immagine6.src= "img/6.png"
+        immagine6.className = "card"
+        immagine6.id= "6d"
+
+        const immagine7 = document.createElement("img")
+        immagine7.src= "img/7d.png"
+        immagine7.className = "card"
+        immagine7.id= "7d"
+
+        const immagine8 = document.createElement("img")
+        immagine8.src= "img/8.png"
+        immagine8.className = "card"
+        immagine8.id= "8d"
+
+        const immagine9 = document.createElement("img")
+        immagine9.src= "img/9.png"
+        immagine9.className = "card"
+        immagine9.id= "9d"
+
+        const immagine10 = document.createElement("img")
+        immagine10.src= "img/10.png"
+        immagine10.className = "card"
+        immagine10.id= "10d"
+
+        const immagine1c = document.createElement("img")
+        immagine1c.src= "img/1c.png"
+        immagine1c.className = "card"
+        immagine1c.id= "1c"
+
+        const immagine2c = document.createElement("img")
+        immagine2c.src= "img/2c.png"
+        immagine2c.className = "card"
+        immagine2c.id= "2c"
+
+
+        const immagine3c = document.createElement("img")
+        immagine3c.src= "img/3c.png"
+        immagine3c.className = "card"
+        immagine3c.id= "3c"
+
+        const immagine4c = document.createElement("img")
+        immagine4c.src= "img/4c.png"
+        immagine4c.className = "card"
+        immagine4c.id= "4c"
+
+        const immagine5c = document.createElement("img")
+        immagine5c.src= "img/5c.png"
+        immagine5c.className = "card"
+        immagine5c.id= "5c"
+
+        const immagine6c = document.createElement("img")
+        immagine6c.src= "img/6c.png"
+        immagine6c.className = "card"
+        immagine6c.id= "6c"
+
+        const immagine7c = document.createElement("img")
+        immagine7c.src= "img/7c.png"
+        immagine7c.className = "card"
+        immagine7c.id= "7c"
+
+        const immagine8c = document.createElement("img")
+        immagine8c.src= "img/8c.png"
+        immagine8c.className = "card"
+        immagine8c.id= "8c"
+
+        const immagine9c = document.createElement("img")
+        immagine9c.src= "img/9c.png"
+        immagine9c.className = "card"
+        immagine9c.id= "9c"
+
+        const immagine10c = document.createElement("img")
+        immagine10c.src= "img/10c.png"
+        immagine10c.className = "card"
+        immagine10c.id= "10c"
+
+        const immagine1b = document.createElement("img")
+        immagine1b.src= "img/1b.png"
+        immagine1b.className = "card"
+        immagine1b.id= "1b"
+
+        const immagine2b = document.createElement("img")
+        immagine2b.src= "img/2b.png"
+        immagine2b.className = "card"
+        immagine2b.id= "2b"
+
+        const immagine3b = document.createElement("img")
+        immagine3b.src= "img/3b.png"
+        immagine3b.className = "card"
+        immagine3b.id= "3b"
+
+        const immagine4b = document.createElement("img")
+        immagine4b.src= "img/4b.png"
+        immagine4b.className = "card"
+        immagine4b.id= "4b"
+
+        const immagine5b = document.createElement("img")
+        immagine5b.src= "img/5b.png"
+        immagine5b.className = "card"
+        immagine5b.id= "5b"
+
+        const immagine6b = document.createElement("img")
+        immagine6b.src= "img/6b.png"
+        immagine6b.className = "card"
+        immagine6b.id= "6b"
+
+        const immagine7b = document.createElement("img")
+        immagine7b.src= "img/7b.png"
+        immagine7b.className = "card"
+        immagine7b.id= "7b"
+
+        const immagine8b = document.createElement("img")
+        immagine8b.src= "img/8b.png"
+        immagine8b.className = "card"
+        immagine8b.id= "8b"
+
+        const immagine9b = document.createElement("img")
+        immagine9b.src= "img/9b.png"
+        immagine9b.className = "card"
+        immagine9b.id= "9b"
+
+        const immagine10b = document.createElement("img")
+        immagine10b.src= "img/10b.png"
+        immagine10b.className = "card"
+        immagine10b.id= "10b"
+
+        const immagine1s = document.createElement("img")
+        immagine1s.src= "img/1s.png"
+        immagine1s.className = "card"
+        immagine1s.id= "1s"
+
+        const immagine2s = document.createElement("img")
+        immagine2s.src= "img/2s.png"
+        immagine2s.className = "card"
+        immagine2s.id= "2s"
+
+        const immagine3s = document.createElement("img")
+        immagine3s.src= "img/3s.png"
+        immagine3s.className = "card"
+        immagine3s.id= "3s"
+
+        const immagine4s = document.createElement("img")
+        immagine4s.src= "img/4s.png"
+        immagine4s.className = "card"
+        immagine4s.id= "4s"
+
+        const immagine5s = document.createElement("img")
+        immagine5s.src= "img/5s.png"
+        immagine5s.className = "card"
+        immagine5s.id= "5s"
+
+        const immagine6s = document.createElement("img")
+        immagine6s.src= "img/6s.png"
+        immagine6s.className = "card"
+        immagine6s.id= "6s"
+
+        const immagine7s = document.createElement("img")
+        immagine7s.src= "img/7s.png"
+        immagine7s.className = "card"
+        immagine7s.id= "7s"
+
+        const immagine8s = document.createElement("img")
+        immagine8s.src= "img/8s.png"
+        immagine8s.className = "card"
+        immagine8s.id= "8s"
+
+        const immagine9s = document.createElement("img")
+        immagine9s.src= "img/9s.png"
+        immagine9s.className = "card"
+        immagine9s.id= "9s"
+
+        const immagine10s = document.createElement("img")
+        immagine10s.src= "img/10s.png"
+        immagine10s.className = "card"
+        immagine10s.id= "10s"
+
+class Mazzo{
+
+    constructor(){
+        this.carte = [immagine, immagine2, immagine3, immagine4, immagine5,
+                    immagine6, immagine7, immagine8, immagine9, immagine10,
+                    immagine1c, immagine2c, immagine3c, immagine4c, immagine5c,
+                    immagine6c, immagine7c, immagine8c, immagine9c, immagine10c,
+                    immagine1b, immagine2b, immagine3b, immagine4b, immagine5b,
+                    immagine6b, immagine7b, immagine8b, immagine9b, immagine10b,
+                    immagine1s, immagine2s, immagine3s, immagine4s, immagine5s,
+                    immagine6s, immagine7s, immagine8s, immagine9s, immagine10s,]
+    }
+
+    get getCarte(){
+        return this.carte;
+    }
+
+    set setCarte(carte){
+        this.carte = carte;
+    }
+
+    mischia(){
+        this.carte.sort((a,b) =>  0.5 - Math.random())
+    }
+
+    distribuisci(){
+        const carta = this.carte.pop() // prende l ultima carta e la cancella dal mazzo
+        //const carta = this.carte.shift()   prende la prima carta e la cancella dal mazzo
+       return carta;
+
+    }
+
+    
+    }
+
+    class Mano{
+        carte;
+        numeroPlayer;
+        punteggio;
+
+        constructor(numeroPlayer){
+            this.carte = [];
+            this.numeroPlayer = numeroPlayer;
+            this.punteggio = [];
+        }
+
+        get getCarte(){
+            return this.carte;
+        }
+
+        set setCarte(carte){
+            this.carte=carte;
+        }
+
+        get getNumeroPlayer(){
+            return this.numeroPlayer;
+        }
+
+        set setNumeroPlayer (numeroPlayer){
+            this.numeroPlayer = numeroPlayer;
+        }
+
+        aggiungiCarta(carta){
+            this.carte.push(carta);
+        }
+
+        aggiungiCartaAlPunteggio(carta){
+            this.punteggio.push(carta);
+        }
+    }
+
+
+    let mazzo = new Mazzo();
+
+    let player1 = new Mano(1);
+
+    let player2 = new Mano(2);
+
+    var banco = [];
+
+    var turno = 1;
+
+    var  conteggioCarteButtate = 0;
+
+
+
+    function inizio(){
+
+
+       daiCarte();      
+        
+        
+        // RECUPERO TUTTE LE CARTE PRESENTI
+        var cards = document.querySelectorAll(".card");
+
+        // PER CIASCUNA CARTA AGGIUNGO UN EVENTO AL CLICK CHE RICHIAMA LA FUNZIONE cardClicked
+        cards.forEach(function (item, index) {
+            cards[index].addEventListener("click", cardClicked);
+        });
+
+
+    }
+
+
+    function daiUnaSolaMano (player){
+        mazzo.mischia();
+    
+        // RIPETO FINO AD ARRIVARE A 3 CARTE
+        for (var i = 0; i < 3; i++) {
+
+            // PRELEVO CASUALMENTE LA POSIZIONE DI UNA CARTA
+            var carta = mazzo.distribuisci();
+
+            // AGGIUNGO LA CARTA al player
+            player.aggiungiCarta(carta);
+
+            // MOSTRO LE CARTE SUL piatto DEL GIOCATORE
+            document.querySelector("#Player" + player.getNumeroPlayer).appendChild(carta);
+
+        }
+    }
+
+    function daiCarte(){
+
+        if(banco.length == 0){
+
+            let divBanco = document.getElementById('banco')
+
+            for(let i=0 ; i < 4 ; i++){
+                
+                let cartaprova = mazzo.distribuisci();
+
+                 banco.push(cartaprova);
+
+                divBanco.appendChild(cartaprova);
+
+            }
+        }
+
+        if(conteggioCarteButtate == 0){
+
+            //distribuisce mano 1
+            daiUnaSolaMano(player1);
+            //distribuisce mano 2
+            daiUnaSolaMano(player2);  
+        }
+
+        
+    }
+
+
+
+    // QUESTA FUNZIONE VIENE RICHIAMATA OGNI VOLTA CHE VIENE CLICCATA UNA CARTA DA UN PIATTO
+function cardClicked(event) {
+
+    //prendere div in cui si trova l immagine che selezioniamo
+    var div = event.target.parentNode
+    //prendere id del div preso precedentemente
+    var idDiv = div.id
+
+    //prendere l ultima numero del id che sarebbe il numero del player che ha giocato
+    let len = idDiv.length;
+    numeroPlayer = idDiv.substring(len-1);
+
+
+    //controllare se il player che vuole giocare è il suo turno
+    if(turno == numeroPlayer){
+
+        conteggioCarteButtate++;
+
+        if(conteggioCarteButtate >5){
+            conteggioCarteButtate=0;
+        }
+    
+        
+        //controllo se con la carta buttata possiamo prendere qualcosa
+        if(!controllo(event.target)){
+
+            //aggiungere carta cliccata al banco
+            document.querySelector("#banco").appendChild(event.target)
+
+            //aggiungere la carta ad un array banco solo se non prendiamo niente per poi fare una verifica in seguito
+            banco.push(event.target);
+        }
+
+        // RIMUOVO IL LISTENER SULLA CARTA IN MODO CHE NON SIA PIÙ SPOSTABILE
+        event.target.removeEventListener("click", cardClicked);
+        turno ++
+
+    }else{
+        alert("non e il tuo turno")
+    }
+
+    // SE HO SUPERATO I due GIOCATORI RICOMINCIO IL GIRO
+    if (turno > 2) {
+        turno = 1;
+    }
+
+}
+
+//controllare se nel banco possiamo prendere qualcosa
+function controllo (cartaDaControllare){
+   
+    //trasforma la carta che buttiamo in array
+    let cartaTransformata = transforma(cartaDaControllare);
+
+    for(let i=0; i<banco.length; i++){
+
+        //trasforma le carte del banco in array
+       let cartaBanco = transforma(banco[i]);
+       
+       //verifica se la carta del banco è uguale alla carta che abbiamo buttato
+        if(cartaTransformata[0] == cartaBanco[0]){
+
+            //prendo il player che ha buttato la carta
+            let divId = cartaDaControllare.parentNode.id
+            let nPlayer = divId.substring(divId.length-1)
+
+            if(nPlayer == 1){
+                console.log("il giocatore 1 ha presso delle carte")
+                player1.aggiungiCartaAlPunteggio(cartaTransformata);
+                player1.aggiungiCartaAlPunteggio(cartaBanco);
+
+            }else{
+                console.log("il giocatore 2 ha presso delle carte")
+                player2.aggiungiCartaAlPunteggio(cartaTransformata);
+                player2.aggiungiCartaAlPunteggio(cartaBanco);
+            }
+            
+            //eliminare carta buttata dal centro
+            let cartaEliminare = document.getElementById(cartaDaControllare.id);
+            cartaEliminare.remove();
+
+            console.log("eliminata carta buttata")
+
+            //elimina carta del banco
+            let bancoElimina = document.getElementById(banco[i].id)
+            bancoElimina.remove();
+
+            //elimina carta dal array banco
+            banco.splice(i, 1);
+
+            console.log("eliminata carta banco")
+
+            return true;
+        }
+    }
+}
+
+//trasforma id in valore e seme e metterli in un array
+function transforma (immagine){
+    let id = immagine.id;
+    let valore = id.slice(0, -1);
+    let seme = id.substr(-1);
+    let carta = [valore, seme];
+    return carta;
+}
+
+
+function vincitore(){
+    let punteggioFinale1 = calcolaPunteggio(player1);
+    let punteggioFinale2 = calcolaPunteggio(player2);
+
+    if(punteggioFinale1 > punteggioFinale2){
+        return "vince player 1";
+    }if(punteggioFinale2 > punteggioFinale1){
+        return "vince player 2";
+    }else{
+        return "la partita è finita in pareggio";
+    }
+
+}
+
+
+function calcolaPunteggio (player){
+
+    let conteggioPunteggio = 0;
+    let conteggioSette =0;
+    let conteggioCarte = player.punteggio.length
+    let conteggioSetteBello = 0;
+    let conteDenari = 0;
+
+    for(let i=0; i<player.punteggio.length; i++){
+        let punteggioplayer = player.punteggio;
+        if(punteggioplayer[i][0] == 7){
+
+            conteggioSette++;
+
+            if(punteggioplayer[i][1] == 'd'){
+                conteggioSetteBello = 1;
+            }
+        }
+
+        if(punteggioplayer[i][1] == 'd'){
+            conteDenari++
+        }
+    }
+
+    if(conteggioSette >2){
+        conteggioPunteggio++
+    }
+
+    if(conteggioCarte >20){
+        conteggioPunteggio++
+    }
+    if(conteDenari>5){
+        conteggioPunteggio++
+    }
+    if(conteggioSetteBello ==1){
+        conteggioPunteggio++
+    }
+
+    console.log(conteggioPunteggio)
+    return conteggioPunteggio;
+}
+
+    
+
